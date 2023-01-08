d56600974b19a544ec2e5810aa93ec4d47a5ee73
Alec Smith <alecmsmith18@gmail.com>
2023-01-08 13:34:09 -0600

    Created Header

--- chat/urls.py
+++ chat/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 
     # Login-section
     path('auth/login/', LoginView.as_view(template_name='chat/LoginPage.html'), name='login-user'),
-    path('auth/logout/', LogoutView.as_view(), name='logout-user')
+    path('auth/logout/', LogoutView.as_view(), name='logout-user'),
+    path('register/', chat_views.register_request, name='register')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from chat.forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 # Create your views here.
 def chatPage(request, *args, **kwargs):
@@ -6,3 +10,15 @@ def chatPage(request, *args, **kwargs):
         return redirect('login-user')
     context = {}
     return render(request, 'chat/chatPage.html', context)
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("chat-page")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render (request=request, template_name="chat/register.html", context={"register_form":form})
